6a8a5ba197958279e1ed935f3c9606d48c6e1f18
Charles Loureiro de Deus <charliedeus@gmail.com>
2020-07-11 15:57:16 -0300

    Create a Default Boilerplate to Project

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          React Avançado!
         </h1>
 
         <p className="description">
