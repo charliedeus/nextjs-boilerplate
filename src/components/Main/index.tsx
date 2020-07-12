b8e18fc0c9656930d658a3478d9467af9739a638
Charles Loureiro de Deus <charliedeus@gmail.com>
2020-07-12 12:07:49 -0300

    Styling first component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
+import {
+ Wrapper, Logo, Title, Description, Illustration
+} from './styles';
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <Wrapper>
+    <Logo
+      src="/images/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado"
+    />
+    <Title>React Avançado</Title>
+    <Description>TypeScript, ReactJS, NextJS e Styled Components</Description>
+    <Illustration
+      src="/images/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código"
+    />
+  </Wrapper>
 );
 
 export default Main;
